8fc8395aef647400cc433c8a52ba740e4be1c038
domingosl <ing.domingo.sl@gmail.com>
2023-03-02 17:06:32 +0100

    Added queue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const childProcess = require('child_process');
-
+const fastq = require('fastq');
 
 const githubSign = require('./lib/github-sign');
 const configManager = require('./lib/config');
@@ -11,7 +11,7 @@ const config = configManager.load();
 const app = express();
 app.use(bodyParser.json());
 
-const runningProcess = {};
+const queues = {};
 const allowedTimeout = 60 * 10 * 1000;
 
 app.post('/app/:appName', configManager.mw, githubSign.mw, async (req, res) => {
@@ -24,37 +24,42 @@ app.post('/app/:appName', configManager.mw, githubSign.mw, async (req, res) => {
 
     const appName = req.params.appName;
 
-    logger("Processing...", { app: appName, cmd });
+    if(!queues[appName])
+        queues[appName] = fastq.promise(async (data) => {
+            //Worker
+            logger("Processing...", { app: data.appName, cmd: data.cmd });
+            data.startedAt = new Date().getTime();
 
-    if(runningProcess[appName] && runningProcess[appName].running)
-        return logger("Cannot created a new flow since one is already running", { app: appName, startedAt: runningProcess[appName].startedAt  });
+            const ciProcess = childProcess.exec(data.cmd);
+            ciProcess.stdout.pipe(process.stdout);
 
-    runningProcess[appName] = {
-        running: true,
-        startedAt: new Date().getTime(),
-        watchdogTimer: setTimeout(()=> {
+            data.watchdogTimer = setTimeout(()=> {
 
-            appName.kill();
-            logger("The flow has been killed for exeiding the timeout", { app: appName, timeout: allowedTimeout })
+                ciProcess.kill();
+                logger("The flow has been killed for exceeding the timeout", { app: data.appName, timeout: allowedTimeout })
 
-        }, allowedTimeout)
-    };
+            }, allowedTimeout);
 
-    const ciProcess = childProcess.exec(cmd);
-    ciProcess.stdout.pipe(process.stdout);
-
-
-    ciProcess.on('exit', function(code) {
+            ciProcess.on('exit', function(code) {
 
 
-        clearTimeout(runningProcess[appName].watchdogTimer);
-        runningProcess[appName].watchdogTimer = undefined;
-        runningProcess[appName].running = false;
-        runningProcess[appName].endedAt = new Date().getTime();
+                clearTimeout(data.watchdogTimer);
+                data.endedAt = new Date().getTime();
 
-        logger('App process exited', { code, 'executionTime[ms]': runningProcess[appName].endedAt - runningProcess[appName].startedAt });
+                logger('App process exited', { code, 'executionTime[ms]': data.endedAt - data.startedAt });
 
-    });
+            });
+
+        }, 1);
+
+    logger('Pushing into queue', { app: appName })
+    await queues[appName].push({
+        appName,
+        cmd,
+        queuedAt: new Date().getTime(),
+
+    })
+
 
 });
 
